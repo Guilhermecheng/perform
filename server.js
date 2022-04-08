d2f0aa816a38a326a41c78475f073bd467c404e6
Guilhermecheng <62719629+Guilhermecheng@users.noreply.github.com>
2022-04-08 00:18:39 -0300

    React perform 1.0

---
+++ server.js
@@ -0,0 +1,16 @@
+
+module.exports = () => {
+    const data = {
+        products: [],
+    }
+
+    for (let i = 0; i < 1000; i++) {
+        data.products.push({
+            id: 1 + 1,
+            price: 80,
+            title: `Camiseta ${i + 1}`
+        })
+    }
+
+    return data;
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import { FormEvent, useState } from 'react';
 import './App.css';
+import { SearchResults } from './components/SearchResults';
 
 function App() {
+  const [search, setSearch] = useState('');
+  const [results, setResults] = useState([]);
+
+  async function handleSearch(event: FormEvent) {
+    event.preventDefault();
+
+    if(!search.trim()) {
+      return;
+    }
+
+    const response = await fetch(`http://localhost:3333/products?q=${search}`)
+    const data = await response.json()
+
+    setResults(data);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Search</h1>
+
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          value={search}
+          onChange={e => setSearch(e.target.value)} 
+        />
+
+        <button type="submit">Buscar</button>
+      </form>
+
+      <SearchResults results={results} />
     </div>
   );
 }
